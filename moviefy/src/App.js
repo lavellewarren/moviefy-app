b0bd5b9bb988d21eea9efc91f9de27c758ba5b13
TheNoodleMoose <cjh605@gmail.com>
2019-01-13 10:18:07 -0500

    Adding first movie components

--- moviefy/src/App.js
+++ moviefy/src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
+
+import Movie from "./Components/Movie";
+
+const movies = [
+  {
+    id: 1,
+    title: "Star Wars"
+  },
+  {
+    id: 2,
+    title: "Spider Man"
+  },
+  {
+    id: 3,
+    title: "36th Chamber of Shaolin"
+  },
+  {
+    id: 4,
+    title: "The Good The Bad and The Ugly"
+  }
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>MOVIEFY</h1>
         </header>
+        {movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
       </div>
     );
   }
