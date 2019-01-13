ea382c16127cabcb6d808334b18a06a6bb725d8c
TheNoodleMoose <cjh605@gmail.com>
2019-01-13 17:10:48 -0500

    Added eslint config

--- moviefy/src/App.js
+++ moviefy/src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./App.css";
+import './App.css';
 
-import Movie from "./Components/Movie";
+import Movie from './Components/Movie';
 
 class App extends Component {
   state = {
-    movies: []
+    movies: [],
   };
 
   async componentDidMount() {
     try {
       const result = await fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=2a208bd9217b421273ab1506025ce6e6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+        'https://api.themoviedb.org/3/discover/movie?api_key=2a208bd9217b421273ab1506025ce6e6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
       );
       const movies = await result.json();
       this.setState({
-        movies: movies.results
+        movies: movies.results,
       });
     } catch (err) {
       console.log(err);
@@ -24,12 +24,13 @@ class App extends Component {
   }
 
   render() {
+    const { movies } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <h1>MOVIEFY</h1>
         </header>
-        {this.state.movies.map(movie => (
+        {movies.map(movie => (
           <Movie key={movie.id} movie={movie} />
         ))}
       </div>
