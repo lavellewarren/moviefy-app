371aace138c99182c8cf54bb2a3039754d3a4685
TheNoodleMoose <cjh605@gmail.com>
2019-01-14 12:19:02 -0500

    Got search feature working

--- moviefy/src/Components/MoviesList.js
+++ moviefy/src/Components/MoviesList.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import Movie from './Movie';
 
-class MoviesList extends Component {
+class MoviesList extends PureComponent {
   state = {
     movies: [],
+    movieSearch: '',
   };
 
   async componentDidMount() {
@@ -21,14 +22,43 @@ class MoviesList extends Component {
     }
   }
 
+  updateInput = (event) => {
+    this.setState({
+      movieSearch: event.target.value,
+    });
+  };
+
+  Search = async () => {
+    const { movieSearch } = this.state;
+    try {
+      const result = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=2a208bd9217b421273ab1506025ce6e6&query=${movieSearch}`,
+      );
+      const movies = await result.json();
+
+      this.setState({
+        movies: movies.results,
+        movieSearch: '',
+      });
+    } catch (err) {
+      throw err;
+    }
+  };
+
   render() {
-    const { movies } = this.state;
+    const { movies, movieSearch } = this.state;
     return (
-      <MovieGrid>
-        {movies.map(movie => (
-          <Movie key={movie.id} movie={movie} />
-        ))}
-      </MovieGrid>
+      <div>
+        <MovieSearchInput type="text" onChange={this.updateInput} value={movieSearch} />
+        <SearchButton type="submit" onClick={this.Search}>
+          Search
+        </SearchButton>
+        <MovieGrid>
+          {movies.map(movie => (
+            <Movie key={movie.id} movie={movie} />
+          ))}
+        </MovieGrid>
+      </div>
     );
   }
 }
@@ -40,4 +70,19 @@ const MovieGrid = styled.div`
   padding: 1rem;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 1rem;
+`;
+
+const MovieSearchInput = styled.input`
+  border: none;
+  height: 20px;
+  border-radius: 10px;
+  margin: 0 5px;
+`;
+
+const SearchButton = styled.button`
+  border: none;
+  color: white;
+  height: 20px;
+  border-radius: 10px;
+  background: #551a8b;
 `;
